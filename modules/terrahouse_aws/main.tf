03b42b71b929f3e30ffa17931e5db2a5083aacd0
ajeet1801 singh <ajeet.singh.mait@gmail.com>
2023-10-14 18:45:24 +0000

    s3 storage

--- modules/terrahouse_aws/main.tf
+++ modules/terrahouse_aws/main.tf
@@ -40,8 +40,8 @@ resource "aws_s3_bucket_website_configuration" "website_configuration" {
 resource "aws_s3_object" "index_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "index.html"
-  source = var.index_html_filepath
-  # source = "${path.root}/public/index.html"
+  #source = var.index_html_filepath
+  source = "${path.root}/public/index.html"
 
   # etag = filemd5(var.index_html_filepath)
 }
@@ -50,7 +50,8 @@ resource "aws_s3_object" "index_html" {
 resource "aws_s3_object" "error_html" {
   bucket = aws_s3_bucket.website_bucket.bucket
   key    = "error.html"
-  source = var.error_html_filepath
+  #source = var.error_html_filepath
+  source = "${path.root}/public/error.html"
 
   # etag = filemd5(var.error_html_filepath)
 }
